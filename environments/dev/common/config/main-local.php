5f1d8a168ff75fe61d8bf797ccd5a8308e3ad0fa
kkpagaev <kapagaev@gmail.com>
2021-12-09 06:57:54 +0200

    docker setup

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=db;dbname=yii2advanced',
             'username' => 'root',
-            'password' => '',
+            'password' => 'verysecret',
             'charset' => 'utf8',
         ],
         'mailer' => [
