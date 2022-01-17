933e95efb8ad1802f2d1e3abb3836ab477e26a62
kkpagaev <kapagaev@gmail.com>
2022-01-17 04:40:14 +0200

    user additional fields

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -11,15 +11,19 @@ class m130524_201442_init extends Migration
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
         }
-
         $this->createTable('{{%user}}', [
             'id' => $this->bigPrimaryKey(),
+            'edrpou' => $this->string(),
             'first_name' => $this->string()->notNull(),
             'last_name' => $this->string()->notNull(),
             'middle_name' => $this->string()->notNull(),
             'email' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32),
             'password' => $this->string()->notNull(),
+            'phone' => $this->string(),
+            'ur_address' => $this->string(),
+            'fact_address' => $this->string(),
+            'delivery_address' => $this->string(),
         ], $tableOptions);
     }
 
