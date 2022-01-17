00903748e9d6f9bb6fc2eabada316854505da872
kkpagaev <kapagaev@gmail.com>
2022-01-17 08:09:25 +0200

    station shipping address relation

--- console/migrations/m220117_032954_create_station_table.php
+++ console/migrations/m220117_032954_create_station_table.php
@@ -12,10 +12,15 @@ class m220117_032954_create_station_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
         $this->createTable('{{%station}}', [
-            'id' => $this->primaryKey(),
+            'id' => $this->bigPrimaryKey(),
             'fullname' => $this->string(),
-        ]);
+        ], $tableOptions);
     }
 
     /**
