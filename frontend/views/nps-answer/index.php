8ab13f5f30a455c1bf2a9ba47b18766d364f0ea8
kkpagaev <kapagaev@gmail.com>
2022-01-27 15:00:39 +0200

    nps pre page

--- frontend/views/nps-answer/index.php
+++ frontend/views/nps-answer/index.php
@@ -1,9 +1,100 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>nps-answer/index</h1>
+<div class="accordion-container">
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+    <div class="accordion-header" id="withIconOne">
+        <div class="card-body">
+
+            <div class="text-left"><h3>Заздалегідь дякуємо Вам за сприяння в поліпшенні нашого продукту і сервісу. Ми глибоко цінуємо Ваш зворотний зв'язок</h3>
+
+                <h4>Будуть потрібні 2-3 хвилини, щоб пройти опитування</h4>
+                <h6>Щоб почати опитування, оберіть мову зі списку: </h6></div>
+
+
+        </div>
+
+    </div>
+
+    <!-- Card start -->
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <div class="card bg-ara">
+            <div class="card-header">
+                <br>
+                <br>
+                <div class="card-title text-center"></div>
+
+            </div
+            >
+
+
+            <br>
+
+            <div class="card-body bg-ara ">
+
+                <div class="accordion-container">
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
+                            <div class="country">
+                                <div class="country-details">
+                                    <a href="/nps-answer/create">
+
+                                        <img src="flags/4x3/ua.svg" alt="Country" /></a>
+                                    <div class="country-name">
+                                        <div class="name">Українська</div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                        <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
+
+                            <div class="country">
+                                <div class="country-details">
+                                    <a href="/nps-answer/create?lang=ru">
+                                        <img src="flags/4x3/ru.svg" alt="Country" /></a>
+                                    <div class="country-name">
+                                        <div class="name">Російська&nbsp;&nbsp;&nbsp;</div>
+
+                                    </div>
+                                </div>	</div>
+                        </div>	</div>
+                </div>
+
+
+
+            </div>
+        </div>
+        <!-- Card end -->
+    </div>
+    <!-- Row start -->
+    <div class="row gutters">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="accordion" id="withIconsAccordion">
+
+
+
+
+
+
+                <div class="card-body">
+                    <div class="text-center"><font color="#####"><i></i></font>
+                    </div>
+                </div>			</div>
+            <div class="card-body">
+                <a href="nps-1.html"><div class="text-center">	<button class="btn btn-primary btn-lg" type="submit">Почати опитування</button></a>	</div>			</div>
+    </div>
+
+
+    <!-- Row end -->
+
+
+
+
+    <!-- Buttons -->
+
+
+</div>
