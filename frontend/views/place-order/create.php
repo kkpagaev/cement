0ecca27fb1fec734d6e6716908af5c149d6f9ff2
kkpagaev <kapagaev@gmail.com>
2022-11-08 08:57:19 +0200

    refactor

--- frontend/views/place-order/create.php
+++ frontend/views/place-order/create.php
@@ -1,6 +1,8 @@
 <?php
 
+use common\models\forms\OrderForm;
 use common\models\one_c\Consignee;
+use common\models\one_c\Order;
 use common\models\one_c\Product;
 use yii\widgets\ActiveForm;
 
@@ -32,169 +34,70 @@ echo $this->render('_heading', [
     'model' => $model,
 ]);
 ?>
-<?php if ($model->scenario == \common\models\forms\OrderForm::SCENARIO_AUTO) : ?>
-    <?php
-    $form = ActiveForm::begin([
-        'id' => 'auto-form',
-        'options' => ['class' => 'form-horizontal'],
-    ]);
-    ?>
-    <input type="hidden" id="delivery_type" value="0">
-    <div class="row gutters">
-        <div class="container h-100">
-            <div class="row h-100 justify-content-center align-items-center">
-                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">
-                    <div class="accordion align-center" id="withIconsAccordion">
-                        <?php
-                        echo $this->render('_customer-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-                        <?php
-                        echo $this->render('_order-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-                        <?php
-                        echo $this->render('_shipping-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-                    </div>
+
+<?php
+$form = ActiveForm::begin([
+    'id' => 'auto-form',
+    'options' => ['class' => 'form-horizontal'],
+]);
+$model->errorSummary($form);
+?>
+<input value="<?php
+                switch ($model->scenario) {
+                    case OrderForm::SCENARIO_AUTO:
+                        echo "0";
+                        break;
+
+                    case OrderForm::SCENARIO_SELF_PICKUP:
+                        echo "1";
+                        break;
+                    case OrderForm::SCENARIO_RAILWAY:
+                        echo "2";
+                        break;
+                } ?>" type="hidden" id="delivery_type" name="delivery_type" />
+<div class="row gutters">
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">
+                <div class="accordion align-center" id="withIconsAccordion">
+                    <?php
+                    echo $this->render('_customer-details', [
+                        'model' => $model,
+                        'form' => $form,
+                    ]);
+                    ?>
+
+                    <?php
+                    echo $this->render('_order-details', [
+                        'model' => $model,
+                        'form' => $form,
+                    ]);
+                    ?>
+
+                    <?php
+                    echo $this->render('_shipping-details', [
+                        'model' => $model,
+                        'form' => $form,
+                    ]);
+                    ?>
                 </div>
             </div>
         </div>
     </div>
-    <?php
-    echo $this->render('_invoice-need', [
-        'model' => $model,
-    ]);
-    ?>
-
-    <?php
-    echo $this->render('_button', [
-        'model' => $model,
-    ]);
-    ?>
-    <?php
-    $form->errorSummary($model);
-    ActiveForm::end() ?>
-<?php endif; ?>
-<!-- Row end -->
-<?php if ($model->scenario == \common\models\forms\OrderForm::SCENARIO_SELF_PICKUP) : ?>
-    <!-- Row start -->
-    <!-- Row start -->
-    <?php
-    $form = ActiveForm::begin([
-        'id' => 'auto-form',
-        'options' => ['class' => 'form-horizontal'],
-    ]);
-    $model->errorSummary($form);
-    ?>
-    <div class="row gutters">
-        <div class="container h-100">
-            <div class="row h-100 justify-content-center align-items-center">
-                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">
-                    <div class="accordion align-center" id="withIconsAccordion">
-                        <?php
-                        echo $this->render('_customer-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-
-                        <?php
-                        echo $this->render('_order-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-
-                        <?php
-                        echo $this->render('_shipping-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php
-    echo $this->render('_invoice-need', [
-        'model' => $model,
-    ]);
-    ?>
-    <?php
-    echo $this->render('_button', [
-        'model' => $model,
-    ]);
-    ?>
-    <?php
-    $form->errorSummary($model);
-    ActiveForm::end() ?>
-<?php endif; ?>
-<!-- Row end -->
-<?php if ($model->scenario == \common\models\forms\OrderForm::SCENARIO_RAILWAY) : ?>
-
-    <!-- Row start -->
-    <!-- Row start -->
-    <?php
-    $form = ActiveForm::begin([
-        'id' => 'auto-form',
-        'options' => ['class' => 'form-horizontal'],
-    ]);
-    ?>
-    <input type="hidden" id="delivery_type" value="1">
-    <div class="row gutters">
-        <div class="container h-100">
-            <div class="row h-100 justify-content-center align-items-center">
-                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">
-                    <div class="accordion align-center" id="withIconsAccordion">
-
-                        <?php
-                        echo $this->render('_customer-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-
-                        <?php
-                        echo $this->render('_order-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-                        <?php
-                        echo $this->render('_shipping-details', [
-                            'model' => $model,
-                            'form' => $form,
-                        ]);
-                        ?>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php
-    echo $this->render('_invoice-need', [
-        'model' => $model,
-    ]);
-    ?>
-    <?php
-    echo $this->render('_button', [
-        'model' => $model,
-    ]);
-    ?>
-    <?php
-    $form->errorSummary($model);
-    ActiveForm::end() ?>
-<?php endif; ?>
+</div>
+<?php
+echo $this->render('_invoice-need', [
+    'model' => $model,
+]);
+?>
+<?php
+echo $this->render('_button', [
+    'model' => $model,
+]);
+?>
+<?php
+$form->errorSummary($model);
+ActiveForm::end() ?>
 
 <br>
 <br>
