cff81a0a086a55c5c15d2f50ebc5e2089075cf20
kkpagaev <kapagaev@gmail.com>
2022-11-08 08:44:05 +0200

    _shippin-details refactor

--- frontend/views/place-order/create.php
+++ frontend/views/place-order/create.php
@@ -57,56 +57,12 @@ echo $this->render('_heading', [
                             'form' => $form,
                         ]);
                         ?>
-                        <div class="accordion-container">
-                            <div class="accordion-header" id="withIconThree">
-
-                                <i class="icon icon-info2"></i>&nbsp;&nbsp;Дані доставки
-                                </a>
-                            </div>
-                            <div id="collapseWithIconThree" class="collapse show" aria-labelledby="withIconThree" data-parent="#withIconsAccordion">
-                                <div class="accordion-body">
-
-                                    <div class="row gutters">
-                                        <div class="col-xl-6 col-lglg-4 col-md-4 col-sm-4 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Особа відповідальна за прийом та
-                                                    розвантаження</label>
-
-                                                <div class="form-group">
-                                                    <?= $form->field($model->order, 'consignee_fullname', [
-                                                        'inputOptions' => [
-                                                            'placeholder' => 'П.І.Б.',
-                                                            'class' => 'form-control',
-                                                        ]
-                                                    ])->label(false) ?>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xl-6 col-lglg-4 col-md-4 col-sm-4 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Контактний телефон відповідальної особи </label>
-
-
-                                                <div class="form-group">
-                                                    <?= $form->field($model->order, 'consignee_phone', [
-                                                        'inputOptions' => [
-                                                            'placeholder' => '+380___ __ __',
-                                                            'class' => 'form-control',
-
-                                                        ]
-                                                    ])->label(false) ?>
-                                                </div>
-                                            </div>
-                                        </div>
-
-
-                                    </div>
-
-
-                                </div>
-                            </div>
-                        </div>
+                        <?php
+                        echo $this->render('_shipping-details', [
+                            'model' => $model,
+                            'form' => $form,
+                        ]);
+                        ?>
                     </div>
                 </div>
             </div>
@@ -157,81 +113,12 @@ echo $this->render('_heading', [
                         ]);
                         ?>
 
-                        <div class="accordion-container">
-                            <div class="accordion-header" id="withIconThree">
-
-                                <i class="icon icon-info2"></i>&nbsp;&nbsp;Дані доставки
-                                </a>
-                            </div>
-                            <div id="collapseWithIconThree" class="collapse show" aria-labelledby="withIconThree" data-parent="#withIconsAccordion">
-                                <div class="accordion-body">
-                                    <div class="row gutters">
-                                        <div class="col-xl-4 col-lglg-4 col-md-4 col-sm-4 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Водій (ПІБ, тф.ном.)</label>
-                                                <div class="form-group">
-                                                    <?= $form->field($model->order, 'driver_info', [
-                                                        'inputOptions' => [
-                                                            'placeholder' => 'П.І.Б.',
-                                                            'class' => 'form-control',
-                                                        ]
-                                                    ])->label(false) ?>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xl-4 col-lglg-4 col-md-4 col-sm-4 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Номер ТЗ</label>
-                                                <div class="form-group">
-                                                    <?= $form->field($model->order, 'driver_car_number', [
-                                                        'inputOptions' => [
-                                                            'placeholder' => 'АЕ____ОК',
-                                                            'class' => 'form-control',
-                                                        ]
-                                                    ])->label(false) ?>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xl-4 col-lglg-4 col-md-4 col-sm-4 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Номер причепу</label>
-                                                <div class="form-group">
-                                                    <?= $form->field($model->order, 'driver_car_trailer_number', [
-                                                        'inputOptions' => [
-                                                            'placeholder' => 'АЕ____ОК',
-                                                            'class' => 'form-control',
-
-                                                        ]
-                                                    ])->label(false) ?>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xl-4 col-lglg-4 col-md-4 col-sm-4 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Адреса розвантаження</label>
-                                                <div class="form-group">
-
-                                                    <div class="form-group">
-                                                        <?= $form->field($model->order, 'unload_address', [
-                                                            'inputOptions' => [
-                                                                'class' => 'form-control',
-
-                                                            ]
-                                                        ])->label(false) ?>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                    </div>
-
-
-                                </div>
-                            </div>
-                        </div>
+                        <?php
+                        echo $this->render('_shipping-details', [
+                            'model' => $model,
+                            'form' => $form,
+                        ]);
+                        ?>
                     </div>
                 </div>
             </div>
@@ -282,91 +169,13 @@ echo $this->render('_heading', [
                             'form' => $form,
                         ]);
                         ?>
+                        <?php
+                        echo $this->render('_shipping-details', [
+                            'model' => $model,
+                            'form' => $form,
+                        ]);
+                        ?>
 
-                        <div class="accordion-container">
-                            <div class="accordion-header" id="withIconThree">
-
-                                <i class="icon icon-info2"></i>&nbsp;&nbsp;Дані доставки
-                                </a>
-                            </div>
-                            <div id="collapseWithIconThree" class="collapse show" aria-labelledby="withIconThree" data-parent="#withIconsAccordion">
-                                <div class="accordion-body">
-
-                                    <div class="row gutters">
-
-                                        <div class="col-xl-6 col-lglg-6 col-md-6 col-sm-6 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Вантажоодержувач</label>
-
-                                                <?php echo $form->field($model->order, 'consignee_id')
-                                                    ->dropDownList(
-                                                        (function () {
-                                                            $arr = Consignee::find()->all();
-                                                            $result = [];
-                                                            foreach ($arr as $el) {
-                                                                $result[$el->c1_id] = $el->fullname;
-                                                            }
-
-                                                            return $result;
-                                                        })()
-                                                    )->label(false);
-                                                ?>
-
-                                            </div>
-                                            <div class="form-group">
-
-                                                <label for="inputName">Гілка вантажоодержувача</label>
-
-                                                <div class="form-group">
-                                                    <?= $form->field($model->order, 'consignee_branch', [
-                                                        'inputOptions' => [
-                                                            'class' => 'form-control',
-
-                                                        ]
-                                                    ])->label(false) ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-xl-6 col-lglg-4 col-md-4 col-sm-4 col-12">
-                                            <div class="form-group">
-                                                <label for="inputName">Код вантажоодержувача </label>
-
-
-                                                <div class="form-group">
-                                                    <?= $form->field($model->order, 'consignee_code', [
-                                                        'inputOptions' => [
-                                                            'placeholder' => '32454',
-                                                            'class' => 'form-control',
-
-                                                        ]
-                                                    ])->label(false) ?>
-                                                </div>
-
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="inputName">Графа 7 залізничної накладної (власник вантажу)</label>
-
-                                                <?= $form->field($model->order, 'column_7', [
-                                                    'inputOptions' => [
-                                                        'class' => 'form-control',
-
-                                                    ]
-                                                ])->label(false) ?>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-xl-6 col-lglg-4 col-md-4 col-sm-4 col-12">
-
-                                        </div>
-
-
-
-                                    </div>
-
-
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
